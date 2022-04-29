dd79f5fa986a4672fbb62644ea8322d599bf5def
erika-bonganay <erika.bonganay@codendtheory>
2022-04-29 10:12:22 +0800

    Restful API testing - Get User Id

---
+++ Test Suites/User/Get User Details.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Get User Details</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7257672e-98e0-4a0f-92da-0f389bc3c1e4</testSuiteGuid>
+   <testCaseLink>
+      <guid>48584b4c-1472-4e97-97d9-0df80869ad89</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User/Verify User Details</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
